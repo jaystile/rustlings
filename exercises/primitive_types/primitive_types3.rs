b9a10bc5427e4688422a112591630a7e28a8b96e
jaystile <46078028+jaystile@users.noreply.github.com>
2020-12-02 19:22:49 -0700

    My answers to rustlings

---
+++ exercises/primitive_types/primitive_types3.rs
@@ -0,0 +1,30 @@
+// primitive_types3.rs
+// Create an array with at least 100 elements in it where the ??? is.
+// Execute `rustlings hint primitive_types3` for hints!
+
+fn main() {
+    let a = ["aasdf", "asedf", "fda", "a", "b", "c", "d", 
+    "aasdf", "asedf", "fda", "a", "b", "c", "d",
+    "aasdf", "asedf", "fda", "a", "b", "c", "d",
+    "aasdf", "asedf", "fda", "a", "b", "c", "d",
+    "aasdf", "asedf", "fda", "a", "b", "c", "d",
+    "aasdf", "asedf", "fda", "a", "b", "c", "d",
+    "aasdf", "asedf", "fda", "a", "b", "c", "d",
+    "aasdf", "asedf", "fda", "a", "b", "c", "d",
+    "aasdf", "asedf", "fda", "a", "b", "c", "d",
+    "aasdf", "asedf", "fda", "a", "b", "c", "d",
+    "aasdf", "asedf", "fda", "a", "b", "c", "d",
+    "aasdf", "asedf", "fda", "a", "b", "c", "d",
+    "aasdf", "asedf", "fda", "a", "b", "c", "d",
+    "aasdf", "asedf", "fda", "a", "b", "c", "d",
+    "aasdf", "asedf", "fda", "a", "b", "c", "d",
+    "aasdf", "asedf", "fda", "a", "b", "c", "d",
+    "aasdf", "asedf", "fda", "a", "b", "c", "d"
+    ];
+
+    if a.len() >= 100 {
+        println!("Wow, that's a big array!");
+    } else {
+        println!("Meh, I eat arrays like that for breakfast.");
+    }
+}
